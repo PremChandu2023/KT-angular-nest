1949babe239238c05ee15044c9ca360a83578b70
premchandu <prem@gmail.com>
2023-11-03 14:19:33 +0530

    dsfdsf

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ transform:true,
   forbidNonWhitelisted:false}));
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
